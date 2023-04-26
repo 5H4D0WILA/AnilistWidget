685f72dbcb0db151083a41434d1080f01c98ac45
LawlietAnime <ryusakilawlietgithub@gmail.com>
2023-04-26 19:47:51 +0200

    Initial Commit

---
+++ AnilistWidget/AnilistWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnilistWidgetApp.swift
+//  AnilistWidget
+//
+//  Created by Inumaki on 26.04.23.
+//
+
+import SwiftUI
+
+@main
+struct AnilistWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
